c3328686c5c33bcac695ec5d70a692af8e048bf3
Taylour Kyniston <taylourkyniston19@gmail.com>
2020-09-08 13:37:26 -0700

    Updated with additional JS code

---
+++ Basic JavaScript Projects - Copy/JS/Basic_JavaScript_2.js
@@ -0,0 +1,13 @@
+document.write("\"Be who you are and say what you feel,"
+       + " because those who mind don\'t matter and those who matter don\'t mind.\""
+       + "  " + "-Dr. Seuss");
+
+function displayDate() {
+    document.getElementById("demo").innerHTML = Date();
+}
+
+function myFunction() {
+    var sentence = "I am learning";
+    sentence += " a lot from this book!";
+    document.getElementById("Concatenate").innerHTML = sentence;
+}
